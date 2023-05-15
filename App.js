666359639d461483359d84fb92aac0cd0d287443
Snir4game <sniramsalem1995@gmail.com>
2023-05-15 10:15:50 +0300

    new test

--- App.js
+++ App.js
@@ -1,12 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import {database,auth} from './src/utilis/Firebase-Config';
+import { collection, addDoc } from 'firebase/firestore';
+import Login from './src/screens/Login/login';
+
 
 export default function App() {
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+<Login>
+
+</Login>
   );
 }
 
