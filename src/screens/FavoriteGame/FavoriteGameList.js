b51a3eb7798ad4aacf5b2fc170faadf0183d0f13
Snir4game <sniramsalem1995@gmail.com>
2023-08-03 16:19:42 +0300

    FavGame

--- src/screens/FavoriteGame/FavoriteGameList.js
+++ src/screens/FavoriteGame/FavoriteGameList.js
@@ -1,20 +1,28 @@
 import React, { useState } from 'react';
-import { StyleSheet } from 'react-native';
-import { Avatar, Button, Card,Text } from 'react-native-paper';
+import { StyleSheet,TouchableOpacity } from 'react-native';
+import { Card,Text } from 'react-native-paper';
 const FavoriteGameList = (props) => {
 
-const favGame = props.route.params.favGame
+const favGame = props.route.params.favoriteGame
 
   return (
-  <Card>
+  <Card style={style.Col}>
     <Card.Cover source={{uri:favGame.GameImage}} />
+    <TouchableOpacity>
     <Text style={style.subText}>{favGame.GameName}</Text>
+    </TouchableOpacity>
   </Card>
   )
 }
 const style =StyleSheet.create({
   subText:{
     fontSize:23,fontWeight:"600"
+  },
+  Col:{
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'center',
+    alignItems: 'baseline',
   }
 })
 
